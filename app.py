094785103bea905959128ca1df3f68ddfa456fd3
ASHFran1 <franc.ash94@gmail.com>
2022-08-15 22:31:13 -0500

    Se añaden características a extraer de la URL

--- app.py
+++ app.py
@@ -2,13 +2,12 @@ from flask import Flask, Response, request
 import json
 from yahoo import search_in_yahoo_finance
 
-
 app = Flask(__name__)
 
 
 @app.route('/')
 def main():
-    return 'Hello raiz'
+    return '**PROYECTO CLASE 1 - FLASK, GITHUB Y AZURE**'
 
 
 @app.route('/api/search')
@@ -17,15 +16,17 @@ def search():
 
     financial_info = search_in_yahoo_finance(ticker=ticker)
 
-    return Response(json.dumps({    #dumps= toma el objeto y lo serializa, luego lo envia como respuesta
+    return Response(json.dumps({  # dumps= toma el objeto y lo serializa, luego lo envia como respuesta
         'ticker': ticker,
         'name': financial_info['quotes'][0]['shortname'],
-        'sector': financial_info['quotes'][0]['sector']
-    }), status=200, mimetype='application/json')    # el estado lo define el desarrollador
+        'long name': financial_info['quotes'][0]['longname'],
+        'sector': financial_info['quotes'][0]['sector'],
+        'industry': financial_info['quotes'][0]['industry']
+    }), status=200, mimetype='application/json')  # el estado lo define el desarrollador
 
 
 @app.route('/api/current-price')
-def current_price( ):
+def current_price():
     return '<p>Hello current price</p>'
 
 
